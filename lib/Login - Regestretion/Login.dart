c8161d263eac2db58d7124e708962a1abe6e6f9b
Ahmed Magdy <ahmedmagdy2319@gmail.com>
2021-02-05 02:11:07 +0200

    ahmed

--- lib/Login - Regestretion/Login.dart
+++ lib/Login - Regestretion/Login.dart
@@ -2,7 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:gooey_carousel/gooey_carrousel.dart';
-/////////ahmed
+/////////ahmedahmed ahmed
 class login extends StatefulWidget {
   @override
   _loginState createState() => _loginState();
